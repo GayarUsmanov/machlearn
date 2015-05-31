d98f8e1687be0921387339ef9d35e605622e1376
kuljitvirk <kvirk@Kuljits-MacBook-Air.local>
2015-05-31 15:02:47 -0700

    Implemented very simple set of commands to go from data to neural net function
    Wrote some action items for the next time in README.md

--- kuljit/digitplay.R
+++ kuljit/digitplay.R
@@ -2,10 +2,14 @@
 # 
 # Author: kvirk
 ###############################################################################
+library('neuralnet')
+data <- read.csv("~/GitHub/train.csv")
 
-
-data <- read.csv("~/Downloads/data.csv")
-
-for (i in 1:19 ){
-	image(matrix(data.matrix(data[i,2:785]),nrow=28, byrow=TRUE),col=gray.colors(256))
-}
+# Create the formula: <output>~<Inputs>
+xname <- paste0("pixel",0:783)
+model <- as.formula(paste("label ~ ",paste(xname, collapse= "+")))
+net <- neuralnet(model, data,hidden=0, rep=1, err.fct="sse",threshold=0.01,linear.output=FALSE)
+#for (i in 1:19 )
+#  {
+#	image(matrix(data.matrix(data[i,2:785]),nrow=28, byrow=TRUE),col=gray.colors(256))
+#}
